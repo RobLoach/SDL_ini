86a8de39776fb4384b451b1a82e8cd2e9301695b
Rob Loach <robloach@gmail.com>
2026-06-30 12:47:39 -0400

    Revert "Empty strings don't count as hasKey"
    This reverts commit fc4653404e6707349b0c796a426099b71a182284.

--- SDL_ini.h
+++ SDL_ini.h
@@ -993,8 +993,7 @@ bool INI_HasKey(const SDL_ini *ini, const char *section, const char *key)
     if (!sec) {
         return false;
     }
-    SDL_ini_item * item = INI__find_entry(sec, key);
-    return item != NULL && item->value && item->value[0] != '\0';
+    return INI__find_entry(sec, key) != NULL;
 }
 
 bool INI_HasSection(const SDL_ini *ini, const char *section)
